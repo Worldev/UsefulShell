0d48f570cd26d47ed68e840e14dac11a8696ca68
mikicat <mcomash@gmail.com>
2015-12-12 20:19:57 +0100

    splitting files

--- shell.py
+++ shell.py
@@ -31,6 +31,7 @@ if __name__ == "__main__":
             from ftplib import FTP
             from modules.ftp import *
             from modules.httpd import *
+            from modules.group import *
 
             
         except ImportError:
@@ -41,36 +42,7 @@ if __name__ == "__main__":
             os.mkdir('tmp')
         except FileExistsError:
             pass
-        
-        class Class:
-            """ This class registers a group (e.g. school class) into a txt file.
-            You can adapt the code to register anything you want """
-            try:
-                def __init__(self):
-                    try:
-                        os.mkdir('Classes')
-                    except FileExistsError:
-                        pass
-                    self.teach = input('Tutor: ')
-                    self.email = input('Teacher\'s email: ')
-                    self.number = input('Number of students: ')
-                    def save(self):
-                        self.classe = input('Class: ')
-                        print('---------')
-                        self.file = open('Classes/' + self.classe + '.txt', 'w')
-                        self.file.write('======= Class of %s =======' % self.classe + "\n")
-                        self.file.write("Tutor: " + self.teach + "\n")
-                        self.file.write("Email: " + self.email + "\n")
-                        self.file.write("Number of students: " + self.number)
-                        self.file.close()
 
-                    save(self)
-                    
-                    
-
-            except KeyboardInterrupt:
-                pass
-                
         try:
             if sys.argv[1] == 'http' or sys.argv[1] == 'httpd' or sys.argv[1] == 'server':
                 run()
@@ -403,7 +375,7 @@ You can encrypt a message writing "encrypt <message>"''')
                 delete_content('tmp/shell_log.log')
 
             elif "registrar" and "classe" in shell or "register" and "class" in shell:
-                classe = Classe()
+                classe = Class()
             elif "registrar" and "classes" in shell or "register" and "classes" in shell:
                 try:
                     while True:
