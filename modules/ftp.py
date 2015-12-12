9261a61d2330100092378adc7ac904baa7571b04
mikicat <mcomash@gmail.com>
2015-12-12 20:26:50 +0100

    splitting and adding info

--- modules/ftp.py
+++ modules/ftp.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import logging, logging.handlers
 import logging.config
 from ftplib import FTP
--- shell.py
+++ shell.py
@@ -32,6 +32,7 @@ if __name__ == "__main__":
             from modules.ftp import *
             from modules.httpd import *
             from modules.group import *
+            from modules.encoding import *
 
             
         except ImportError:
@@ -125,19 +126,6 @@ You can encrypt a message writing "encrypt <message>"''')
                     print(hostname, 'no està en línia')
                 else:
                     print(hostname, 'is down!')
-
-        def encode(s):
-            """ This function encodes a message with base64 standard encode """
-            encoded_text = base64.standard_b64encode(s)
-            encoded_string = bytes.decode(encoded_text)
-            print(encoded_string)
-
-        def decode(s):
-            """ This function decodes a message with base64 standard decode """
-            decoded_text = base64.standard_b64decode(s)
-            decoded_string = bytes.decode(decoded_text)
-            print(decoded_string)
-      
 
         def delete_content(filename):
             """ This function deletes the history file's content """
