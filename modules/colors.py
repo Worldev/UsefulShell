664f9759b72caa0555726ded0853272ce5ea1903
mikicat <mcomash@gmail.com>
2015-12-14 22:08:15 +0100

    removed unnecessary function

--- modules/colors.py
+++ modules/colors.py
@@ -7,11 +7,5 @@ class bcolors:
     FAIL = '\033[91m'
     ENDC = '\033[0m'
 
-    def disable(self):
-        self.HEADER = ''
-        self.OKBLUE = ''
-        self.OKGREEN = ''
-        self.WARNING = ''
-        self.FAIL = ''
-        self.ENDC = ''
+
 
