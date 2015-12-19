d5e9c3978d05cf18eac461ce899f60ba696559ed
mikicat <mcomash@gmail.com>
2015-12-19 14:29:09 +0100

    fixing problems

--- test/test.py
+++ test/test.py
@@ -21,4 +21,4 @@ PACKAGE_PARENT = '..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
-from Lib import *
+from lib import *
