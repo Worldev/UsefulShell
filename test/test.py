f0f87e7fd9753f9c43066766ccc3feee80c8f2ac
mikicat <mcomash@gmail.com>
2015-12-19 14:02:03 +0100

    Updatie

--- test/test.py
+++ test/test.py
@@ -17,4 +17,8 @@ import re
 import base64
 import inspect
 import builtins
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
+from modules import *
