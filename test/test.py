1c61f6f7e3efcca75d54f9ca6ad1fa4780a36ab1
mikicat <mcomash@gmail.com>
2015-12-28 17:56:23 +0100

    Deleting unnecessary things

--- test/test.py
+++ test/test.py
@@ -1,22 +1,7 @@
 #!/usr/bin/env python3
 import os
 import sys
-import platform
-import webbrowser
-import time
-import random
-import subprocess
-import logging, logging.handlers
-import logging.config
-import http.server
-import socketserver
-import queue
-import urllib, urllib.parse, urllib.request
-import json
-import re
-import base64
-import inspect
-import builtins
+
 PACKAGE_PARENT = '..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
